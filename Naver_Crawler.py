ec37335af67e8172f3e245623a5a02891255d868
KHK <khk.dblab@gmail.com>
2017-12-02 18:44:35 +0900

    naver news count article number

--- Naver_Crawler.py
+++ Naver_Crawler.py
@@ -1,5 +1,74 @@
 import requests
 import pandas as pd
+import datetime
+from bs4 import BeautifulSoup
+import time
+import random
+
+movie_data = pd.read_csv("./csv/15_final.csv", encoding='utf-8')
+
+movie_name = [i for i in movie_data['movie_name']]
+release_date = [i for i in movie_data['release_date']]
+end_date = [i for i in movie_data['day15_date']]
+
+Movie_date = pd.DataFrame()
+
+Movie_date['movie_name'] = movie_name
+Movie_date['release_date'] = release_date
+Movie_date['day15_date'] = end_date
 
 
-movie_data = pd.read_csv("./csv/15일")
+# 링크 틀
+def url_maker(query, sdate, edate):
+    # base_url = 'http://news.naver.com/main/search/search.nhn?query={}&ds={}&de={}'
+    # base_url = 'https://search.naver.com/search.naver?where=news&query={}&ds={}&de={}'
+    base_url = 'https://search.naver.com/search.naver?where=news&query={}&sm=tab_opt&sort=0&photo=0&field=0&reporter_article=&pd=3&ds={}&de={}'
+    con_sdate = datetime.datetime.strptime(sdate, "%Y-%m-%d").date()
+    con_edate = datetime.datetime.strptime(edate, "%Y-%m-%d").date()
+
+    sdate = con_sdate - datetime.timedelta(days=7)
+    edate = con_edate - datetime.timedelta(days=7)
+
+    sdate = sdate.strftime('%Y.%m.%d')
+    edate = edate.strftime('%Y.%m.%d')
+
+    encoded_query = requests.utils.quote(query)
+    base_url = base_url.format(encoded_query, sdate, edate)
+
+    return base_url
+
+
+# 최종 링크주소 새성
+FinalUrl = []
+for index,row in Movie_date.iterrows():
+    movie_name = getattr(row,'movie_name')
+    release_date = getattr(row,'release_date')
+    end_date = getattr(row,'day15_date')
+
+    url = url_maker(movie_name,release_date,end_date)
+
+    FinalUrl.append(url)
+
+# user-agent 헤더를 넣어야 제한 안걸림
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows; U; MSIE 9.0; WIndows NT 9.0; ko-KR))',
+}
+
+countlist = []
+for url in FinalUrl:
+    try:
+        req = requests.get(url, headers=headers)
+        soup = BeautifulSoup(req.content, 'html.parser')
+        cnt = soup.find('div', class_='title_desc all_my').text
+        cnt = cnt[7:-1]
+        cnt = cnt.replace(',','')
+
+    except Exception as e:
+        print(e)
+        pass
+
+    countlist.append(cnt)
+
+    time.sleep(2 + random.random() * 4)
+
+movie_data['news7'] = countlist
