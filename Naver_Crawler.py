1e257cdcde0a77af521b98b57119d9f1c6681403
KHK <khk.dblab@gmail.com>
2017-12-02 21:50:01 +0900

    네이버 뉴스 크롤링

--- Naver_Crawler.py
+++ Naver_Crawler.py
@@ -6,14 +6,31 @@ import time
 import random
 
 movie_data = pd.read_csv("./csv/15_final.csv", encoding='utf-8')
+director_data = pd.read_csv("./ipynb/15_missing.csv", encoding='utf-8')
 
 movie_name = [i for i in movie_data['movie_name']]
 release_date = [i for i in movie_data['release_date']]
 end_date = [i for i in movie_data['day15_date']]
+director_name = []
+
+for i in movie_name:
+    print(i)
+    director_name.append(director_data['director'][director_data['movieNm'] == i].item())
+
+Search=zip(movie_name,director_name)
+
+Plus=[]
+for i in Search:
+    a = list(i)
+    Plus.append(a)
+
+MovieDirector=[]
+for i in Plus:
+    b=' '.join(i)
+    MovieDirector.append(b)
 
 Movie_date = pd.DataFrame()
-
-Movie_date['movie_name'] = movie_name
+Movie_date['movie_name'] = MovieDirector
 Movie_date['release_date'] = release_date
 Movie_date['day15_date'] = end_date
 
@@ -40,12 +57,12 @@ def url_maker(query, sdate, edate):
 
 # 최종 링크주소 새성
 FinalUrl = []
-for index,row in Movie_date.iterrows():
-    movie_name = getattr(row,'movie_name')
-    release_date = getattr(row,'release_date')
-    end_date = getattr(row,'day15_date')
+for index, row in Movie_date.iterrows():
+    movie_name = getattr(row, 'movie_name')
+    release_date = getattr(row, 'release_date')
+    end_date = getattr(row, 'day15_date')
 
-    url = url_maker(movie_name,release_date,end_date)
+    url = url_maker(movie_name, release_date, end_date)
 
     FinalUrl.append(url)
 
@@ -55,20 +72,28 @@ headers = {
 }
 
 countlist = []
+i = 0
 for url in FinalUrl:
     try:
         req = requests.get(url, headers=headers)
         soup = BeautifulSoup(req.content, 'html.parser')
         cnt = soup.find('div', class_='title_desc all_my').text
         cnt = cnt[7:-1]
-        cnt = cnt.replace(',','')
+        cnt = cnt.replace(',', '')
+        if cnt == '':
+            cnt = '0'
+        i += 1
+        print(cnt + ": %s" % i)
 
     except Exception as e:
+        print(url)
         print(e)
+        cnt = '0'
         pass
-
     countlist.append(cnt)
 
-    time.sleep(2 + random.random() * 4)
+    # time.sleep(2 + random.random() * 4)
 
 movie_data['news7'] = countlist
+
+movie_data.to_csv('./csv/15_final_with_news.csv', encoding='euc-kr')
